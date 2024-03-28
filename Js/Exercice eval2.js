ad791048fbd58aec4d4774a1782ce438c54e503d
Samir <samirb80440@gmail.com>
2024-03-28 13:21:31 +0100

    Eval html

---
+++ Js/Exercice eval2.js
@@ -0,0 +1,10 @@
+function tableMutlplication() {
+    N=prompt("table")
+    var total =0
+    for(var i =0;i <=10;i++)
+    {
+        total= +i * +N
+        console.log(i+"*"+N+"="+total);
+}
+}
+tableMutlplication()
