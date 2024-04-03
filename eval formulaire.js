754cfee041716241599a4455d8ca5c6150a13177
Samir <samirb80440@gmail.com>
2024-04-03 10:14:26 +0200

    nvmodif

--- eval formulaire.js
+++ eval formulaire.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ListeCaract=new RegExp("^[A-Za-z]")
     ListeCodepost=new RegExp("^([0-9]{5})*$")
     ListeEmail=new RegExp("^[a-z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$")
-    ListeNaissance=new RegExp("^[0-9]+\/[0-9]+\/[0-9]+$")
+    ListeNaissance=new RegExp("^[0-9]+\/[0-9]+\/[0-9]+$") 
     test= ListeCaract.test(caract)
     if (caract ==false){
         envoyer=false
